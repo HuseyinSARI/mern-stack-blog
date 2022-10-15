8d89b05342d8efd6ec6f0425ea824c3aee07e807
Hüseyin <huseyin-sari61@hotmail.com>
2022-10-15 12:21:03 +0300

    fix(db connection): changed mongodb connection string and update .env file

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const colors = require('colors');
 const dotenv = require ('dotenv').config();
 
 const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -15,16 +16,24 @@ app.use(express.json({extended : false}));
 
 const connectDB = async () =>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connectted: ${conn.connection.host}`);
+        const conn = await mongoose.connect(
+         `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster-hs.vrot0gz.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+        )
+        console.log(`MongoDB Connected: ${conn.connection.host}`.green.bold);
     } catch (error) {
         console.log(`ERROR : ${error.message}`.red.bold);
         process.exit(1);
-    }
-    
+    }    
 }
 
 connectDB();
 
+// oluşturduğumuz rotaları server'ımıza bağlıyoruz.
+// server'a bu uç dan bir istek geldiğinde useRoutes da oluşturduğumuz rotalara gidecek
+
+// app.use("/api/users", require("./routes/userRoutes"));
+
 
 app.listen(PORT, ()=> console.info(`Server is running on port ${PORT}`.green));
+
+
