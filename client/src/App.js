652b76332abf48a7f95076afffc39af52a3b6572
Hüseyin <huseyin-sari61@hotmail.com>
2022-10-16 19:02:54 +0300

    feat: make routers and privatize some of it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//  #region ---------------[ Import Components ]-----------------
+import BlogDetail from "./pages/BlogDetail";
+import BlogList from "./pages/BlogList";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NewBlog from "./pages/NewBlog";
+import PrivateRoute from "./pages/PrivateRoute";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+// import EditBlog from "./pages/EditBlog";
+//  #endregion ---------------------------------------------------
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+        <Route path='/blogs' element={<PrivateRoute />}>
+          <Route path='/blogs' element={<BlogList />} />
+        </Route>
+        <Route path='/blogs/:id' element={<PrivateRoute />}>
+          <Route path='/blogs/:id' element={<BlogDetail />} />
+        </Route>
+        <Route path='/newblog' element={<PrivateRoute />}>
+          <Route path='/newblog' element={<NewBlog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>);
 }
 
 export default App;
