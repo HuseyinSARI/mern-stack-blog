070b6f46515082f905119f81019f451f8fd97585
Hüseyin <huseyin-sari61@hotmail.com>
2022-10-19 12:02:58 +0300

    feat: first structure for blog list page

--- client/src/pages/BlogList.jsx
+++ client/src/pages/BlogList.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from 'react'
+import {
+  Grid, Typography, Button,
+  Container, Stack, Tooltip,
+  Box, List, ListItemText, Paper, ListItem
+} from "@mui/material"
+import Masonry from '@mui/lab/Masonry';
+import { Link, useNavigate } from 'react-router-dom';
 import MainContainer from '../components/MainContainer'
 import { useBlog } from "../middleware/contextHooks";
-import { toast } from "react-toastify";;
+import { toast } from "react-toastify"; import blogReducer from '../context/blog_context/blogReducer';
+;
 
 function BlogList() {
-  const {getBlogs, toasts, clearErrors, blogs, clearCurrentBlog} = useBlog();
-  
+  const { getBlogs, toasts, clearErrors, blogs, clearCurrentBlog } = useBlog();
+  const navigate = useNavigate();
   const [myBlogs, setMyBlogs] = useState([]);
 
 
@@ -30,7 +38,30 @@ function BlogList() {
 
   return (
     <MainContainer>
-      {myBlogs.length}
+      <Container maxWidth="lg" sx={{ py: 1, my: 1 }} >
+        <Grid container spacing={2}>
+          <Grid item xs={false} md={3}>
+
+            <Stack spacing={2} sx={{ display: "flex" }} direction="row">
+              <Box sx={{ flexGrow: 1 }} />
+              <Button fullWidth={false} onClick={() => navigate("/newBlog")}> Create Blog </Button>
+            </Stack>
+
+            <List sx={{ backgroundColor: "silver", borderRadius:5, mt: 3 }}>
+              {myBlogs.map(blog => (
+                <Link key={blog._id} to={`/blogs/${blog._id}`}>
+                  <ListItem>
+                    <Tooltip title={blog.title} placement="right">
+                      <ListItemText primary={blog.title} />
+                    </Tooltip>
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+            
+          </Grid>
+        </Grid>
+      </Container>
     </MainContainer>
   )
 }
